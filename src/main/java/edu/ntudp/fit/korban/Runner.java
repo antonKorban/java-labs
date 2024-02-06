c3fe9b5fcba9a054a36c3ce484fa7c778c107b63
antonKorban <anton260503@gmail.com>
2024-02-06 17:25:51 +0200

    first change

--- src/main/java/edu/ntudp/fit/korban/Runner.java
+++ src/main/java/edu/ntudp/fit/korban/Runner.java
@@ -2,6 +2,6 @@ package edu.ntudp.fit.korban;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("lab0 korban anton");
+        System.out.println("lab_1 створили гiт ");
     }
 }
